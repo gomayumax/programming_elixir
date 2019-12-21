913a8282863c45bc2539bcac65831f9c20e01d41
ごま <gomayumax@users.noreply.github.com>
2019-12-21 13:41:35 +0900

    5.1

--- 5/function1.exs
+++ 5/function1.exs
@@ -1,2 +1,8 @@
 list_contact = fn (a, b )-> a ++b end
 IO.inspect(list_contact.([:a, :b], [:c, :d]))
+
+sum = fn (a,b,c) -> a+b+c end
+IO.inspect(sum.(1,2,3))
+
+pair_tuple_to_list = fn {a, b} -> [a,b] end
+IO.inspect(pair_tuple_to_list.({124,567}))
